e40c6fa15ba8121698e477fddec6d5e49a283915
cultirg <roxygustine@yahoo.com>
2016-11-04 11:52:53 -0600

    Added some codes and CSS/style

--- js/script.js
+++ js/script.js
@@ -12,11 +12,12 @@ $.ajax({
     var currentRow;
     for(var i = 0; i < asteroids.length; i ++){
 
-      if (i % 3 === 0){
         currentRow = $("<div></div>").addClass("row");
         $(".container").append(currentRow);
-        }
+
         makeName(asteroids[i], currentRow);
+        makeSize(asteroids[i], currentRow);
+        makeClose(asteroids[i], currentRow);
 
       }
     }
@@ -28,6 +29,37 @@ function makeName(asteroids, row) {
   var name = $("<h3></h3>").html(asteroids.name);
 
   $(column).append(name);
+  $(row).append(column);
+}
+
+function makeSize(asteroids, row){
+  var column = $("<div></div>").addClass("col-md-4");
+  // TODO: I should calulate the overage size of the asteroid, rather than just
+  // use it's max diameter
+  var size = $("<h4></h4>").html(asteroids.estimated_diameter.feet.estimated_diameter_max);
+
+  $(column).append(size);
+  $(row).append(column);
+}
+
+// this function is used to create  the third column, which contains close
+// approach data about this asteroid.
+function makeClose(asteroid, row) {
+  var column =$("<div></div>").addClass("col-md-4");
+  var speed =$("<h4></h4>").html(asteroid.close_approach_data[0].relative_velocity.miles_per_hour);
+  var distance =$("<h4></h4>").html(asteroid.close_approach_data[0].miss_distance.miles);
+
+  var hazard =$("<h4></h4>");
+  if (asteroid.is_potentially_hazardous_asteroid){
+    $(hazard).html("Dangerous").addClass("text-danger");
+  } else {
+    $(hazard).html("Safe").addClass("text-success");
+
+  }
+
+  $(column).append(speed);
+  $(column).append(distance);
+  $(column).append(hazard);
   $(row).append(column);
 
 }
